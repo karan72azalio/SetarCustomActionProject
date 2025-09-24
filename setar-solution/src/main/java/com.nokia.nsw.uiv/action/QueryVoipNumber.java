8046b9145d6802c713ad9716a53519d89c3498c0
Karan <karan.kumar@azalio.io>
2025-09-24 17:42:22 +0530

    Setar-Commiting wave 3 custom actions.

--- setar-solution/src/main/java/com.nokia.nsw.uiv/action/QueryVoipNumber.java
+++ setar-solution/src/main/java/com.nokia.nsw.uiv/action/QueryVoipNumber.java
@@ -58,7 +58,7 @@ public class QueryVoipNumber implements HttpAction {
             // Step 2: Service link
             String linkType = (req.getServiceLink() != null && "CBM".equalsIgnoreCase(req.getServiceLink()))
                     ? "CBM" : "ONT";
-            String ontName = "ONT_" + req.getOntSN();
+            String ontName = "ONT" + req.getOntSN();
 
             // Step 3: Prepare empty fields
             String voipNumber1 = "";
